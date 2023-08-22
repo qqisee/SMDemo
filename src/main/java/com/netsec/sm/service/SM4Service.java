9970bb010fbc9d7b8687d502029a9769b9492e57
qqisee <1475906890@qq.com>
2023-08-22 22:20:39 +0800

    service层实现sm4

--- src/main/java/com/netsec/sm/service/SM4Service.java
+++ src/main/java/com/netsec/sm/service/SM4Service.java
@@ -1,5 +1,12 @@
 package com.netsec.sm.service;
 
+
+
 public interface SM4Service {
     byte[] sm4Encrypt(byte[] in,String key);
+
+    byte[] sm4Encrypt(String plainText, String algo, byte[] expectRes) throws Exception;
+
+
+    byte[] sm4Decrypt(byte[] cipherText, String algo,byte[] expectRes) throws Exception;
 }
--- src/test/java/com/netsec/sm/service/impl/SM4ServiceImplTest.java
+++ src/test/java/com/netsec/sm/service/impl/SM4ServiceImplTest.java
@@ -8,19 +8,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SM4ServiceImplTest {
 
+    private static String plainText = "helloworldhellow";  // 16bytes for NoPadding
     @Autowired
     private SM4ServiceImpl sm4Service;
 
     @Test
-    void sm4Encrypt() {
+    void sm4Encrypt() throws Exception {
         byte[] bytes = "1234567890123456".getBytes();
-        byte[] sm4Encrypt = sm4Service.sm4Encrypt(bytes,"11111111".getBytes());
+        byte[] sm4Encrypt = sm4Service.sm4Encrypt(bytes,"8888888888888888");
         System.out.println(ByteUtils.toHexString(sm4Encrypt));
+        byte[] sm4Encrypt1 = sm4Service.sm4Encrypt(plainText, "SM4/CBC/NOPADDING", new byte[]{86, 69, 47, -115, -63, 54, 35, 24, -2, 114, 113, 102, 82, 20, 69, 59});
+        System.out.println(Arrays.toString(sm4Encrypt1));
+
+        byte[] sm4Decrypt = sm4Service.sm4Decrypt(new byte[]{86, 69, 47, -115, -63, 54, 35, 24, -2, 114, 113, 102, 82, 20, 69, 59}, "SM4/CBC/NOPADDING", plainText.getBytes());
+        System.out.println(new String(sm4Decrypt));
+
     }
 
     @Test
